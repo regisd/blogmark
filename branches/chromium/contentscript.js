d75013a431ef501ff3034b45a6a6a3cbb91f8e02
regis.decamps@gmail.com <regis.decamps@gmail.com@8a81f610-6a20-0410-a7af-55ae86ea64fb>
2009-11-27 20:25:11 +0000

    simplified

--- branches/chromium/contentscript.js
+++ branches/chromium/contentscript.js
@@ -1,23 +1,16 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	if (sender.tab != null) {
-		console.log("Message comes from a page, not for me");
-		sendResponse( {
-			blogmark : "ignored"
-		});
-		return;
-	}
 	if (request.blogmark == "new") {
 		console.log("new blogmark for " + document.title);
-
 		var resp = {
 			title : document.title,
 			url : document.location.href,
 			/* encode/decode is the best way to serialize a complex DOMObject */
-			description : decodeURIComponent(encodeURIComponent(document.getSelection())),
+			description : decodeURIComponent(encodeURIComponent(document
+					.getSelection())),
 			via : document.referrer
 		};
 		sendResponse(resp);
 	} else {
-		sendResponse( {	}); // snub them.
+		sendResponse( {}); // snub them.
 	}
 });
